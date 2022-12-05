347f0848b17d70a8bf4328f02d71905382920af1
Emerson M Luz <emerson.maciel@vortigo.com.br>
2022-12-05 20:54:01 -0300

    implementado layout da tela people

---
+++ Movie-Trending-Week/Movie-Trending-Week/Views/CellSetupPeople.swift
@@ -0,0 +1,18 @@
+//
+//  CellSetupPeople.swift
+//  Movie-Trending-Week
+//
+//  Created by Émerson M Luz on 05/12/22.
+//
+
+import UIKit
+
+class CellSetupPeople: UITableViewCell {
+    
+    @IBOutlet weak var peopleImageView: UIImageView!
+    @IBOutlet weak var peopleNameLabel: UILabel!
+    @IBOutlet weak var officeImageView: UIImageView!
+    @IBOutlet weak var officeLabel: UILabel!
+    @IBOutlet weak var popularityImageView: UIImageView!
+    @IBOutlet weak var popularityLabel: UILabel!
+}
