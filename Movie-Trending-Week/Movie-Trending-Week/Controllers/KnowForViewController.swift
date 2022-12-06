7a4aee8dfd49e2d54c9177657478690f1f6d2bbb
Emerson M Luz <emerson.maciel@vortigo.com.br>
2022-12-06 13:44:06 -0300

    esqueleto da tela know for clad

---
+++ Movie-Trending-Week/Movie-Trending-Week/Controllers/KnowForViewController.swift
@@ -0,0 +1,19 @@
+//
+//  KnowForViewController.swift
+//  Movie-Trending-Week
+//
+//  Created by Émerson M Luz on 06/12/22.
+//
+
+import UIKit
+
+class KnowForViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
