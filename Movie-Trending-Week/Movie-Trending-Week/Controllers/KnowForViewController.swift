057504a227ba18ddd41ddf7d8f880518df6fd193
Emerson M Luz <emerson.maciel@vortigo.com.br>
2022-12-06 17:26:33 -0300

    retirado load da tela knowfor

--- Movie-Trending-Week/Movie-Trending-Week/Controllers/KnowForViewController.swift
+++ Movie-Trending-Week/Movie-Trending-Week/Controllers/KnowForViewController.swift
@@ -10,17 +10,11 @@ import UIKit
 class KnowForViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
-    @IBOutlet weak var loadingView: UIView!
-    @IBOutlet weak var loadingActivityIndicator: UIActivityIndicatorView!
     
     var person: Person?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        loadingView.alpha = 0.5
-        
-        LoadingScreen.startLoading(loadingActivity: loadingActivityIndicator, loadingView: loadingView)
         
         tableView.dataSource = self
         tableView.delegate = self
@@ -39,10 +33,6 @@ extension KnowForViewController: UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! CellSetupKnowFor
         
         cell.loadCell(midia: person!, index: indexPath.row)
-        
-        if indexPath.row == person!.actuations.count - 2 {
-            LoadingScreen.stopLoading(loadingActivity: self.loadingActivityIndicator, loadingView: self.loadingView)
-        }
         
         return cell
     }
